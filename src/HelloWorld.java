cbc75101b494c8d676de4b7046174c1bc8324361
Carlos Saul <ccggomez9@gmail.com>
2020-10-28 10:16:09 -0600

    Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
-}
+
+         /* This is my first java program.
+         * This will print 'Hello World' as the output
+         */
+
+        public static void main(String []args) {
+            System.out.println("Hello World"); // prints Hello World
+        }
+    }
+
